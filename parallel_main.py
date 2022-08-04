22358d7edb76a6b150cfe1b09afa3f722a91836a
Louis Manestar <louis.manestar@gmail.com>
2022-08-04 17:02:19 +0100

    Added debugging as an optional flag

--- parallel_main.py
+++ parallel_main.py
@@ -76,6 +76,8 @@ parser.add_argument('--master-port', default=3000, type=int,
                     help='The port for processes to communicate through.')
 parser.add_argument('--init-seed', default=0, type=int,
                     help='Set the manual seed for initialising models.')
+parser.add_argument('--debug-distributed', action='store_true', default=False,
+                    help='Set torch.distributed logging to "DETAILED".')
 
 
 def main(gpu_index: int, args: argparse.Namespace):
@@ -233,7 +235,8 @@ if __name__ == '__main__':
     os.environ['MASTER_ADDR'] = args.master_address
     os.environ['MASTER_PORT'] = str(args.master_port)
 
-    os.environ["TORCH_CPP_LOG_LEVEL"] = "INFO"
-    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
+    if args.debug_distributed:
+        os.environ["TORCH_CPP_LOG_LEVEL"] = "INFO"
+        os.environ["TORCH_DISTRIBUTED_DEBUG"] = "DETAIL"
 
     mp.spawn(main, nprocs=args.number_of_gpus, args=(args,))
