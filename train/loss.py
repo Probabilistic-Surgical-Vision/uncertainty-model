462b1995390a920df89d6dcf5ee497388bcb5c89
Louis Manestar <louis.manestar@gmail.com>
2022-08-16 12:19:54 +0100

    Added commenting to train/loss

--- train/loss.py
+++ train/loss.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Optional
 
 import torch
 import torch.nn as nn
@@ -12,6 +12,15 @@ from . import utils as u
 
 
 class WeightedSSIMLoss(nn.Module):
+    """Calculate the SSIM/L1 loss between two images.
+
+    Args:
+        alpha (float, optional): The weight of the SSIM Loss in the overall
+            metric (note that L1 weight is equal to 1 - alpha).
+            Defaults to 0.85.
+        k1 (float, optional): The first SSIM factor. Defaults to 0.01.
+        k2 (float, optional): The second SSIM factor. Defaults to 0.03.
+    """
     def __init__(self, alpha: float = 0.85, k1: float = 0.01,
                  k2: float = 0.03) -> None:
 
@@ -24,7 +33,18 @@ class WeightedSSIMLoss(nn.Module):
         self.pool = nn.AvgPool2d(kernel_size=3, stride=1)
 
     def ssim(self, x: Tensor, y: Tensor) -> Tensor:
+        """Calculate the per-pixel SSIM between two images.
 
+        Note:
+            Both images are average-pooled and therefore smaller.
+
+        Args:
+            x (Tensor): The first image to compare.
+            y (Tensor): The second image to compare.
+
+        Returns:
+            Tensor: The SSIM image (reduced in size by pooling).
+        """
         luminance_x = self.pool(x)
         luminance_y = self.pool(y)
 
@@ -46,10 +66,34 @@ class WeightedSSIMLoss(nn.Module):
         return numerator / denominator
 
     def dssim(self, x: Tensor, y: Tensor) -> Tensor:
+        """Calculate the Structural Dissimilarity (DSSIM) between two images.
+
+        Note:
+            Both images are average-pooled and therefore smaller.
+
+        Args:
+            x (Tensor): The first image to compare.
+            y (Tensor): The second image to compare.
+
+        Returns:
+            Tensor: The per-pixel DSSIM image (reduced in size by pooling).
+        """
         dissimilarity = (1 - self.ssim(x, y)) / 2
         return torch.clamp(dissimilarity, 0, 1)
 
     def forward(self, images: Tensor, recon: Tensor) -> Tensor:
+        """Calculate the weighted SSIM loss.
+
+        This is given by:
+            loss = (alpha * DSSIM) + ((1 - alpha) * L1)
+
+        Args:
+            x (Tensor): The first image to compare.
+            y (Tensor): The second image to compare.
+
+        Returns:
+            Tensor: The WSSIM loss as a single float.
+        """
         left_l1_loss = u.l1_loss(images[:, 0:3], recon[:, 0:3])
         right_l1_loss = u.l1_loss(images[:, 3:6], recon[:, 3:6])
 
@@ -63,10 +107,27 @@ class WeightedSSIMLoss(nn.Module):
 
 
 class ConsistencyLoss(nn.Module):
+    """Calculate the consistency loss between two disparity images.
+
+    This is achieved by reconstructing each view of the disparity from the
+    opposite image. By comparing the original disparity with the original
+    image, the model learns to output similar disparity maps in both views.
+
+    Based off:
+        https://arxiv.org/abs/1609.03677
+    """
     def __init__(self) -> None:
         super().__init__()
 
     def forward(self, disp: Tensor) -> Tensor:
+        """Calculate the consistency loss of the disparity prediction.
+
+        Args:
+            disp (Tensor): The stereo disparity prediction.
+
+        Returns:
+            Tensor: The consistency loss as a single float.
+        """
         left_lr_disp = u.reconstruct_left_image(disp[:, 0:1], disp[:, 1:2])
         right_lr_disp = u.reconstruct_right_image(disp[:, 1:2], disp[:, 0:1])
 
@@ -77,23 +138,48 @@ class ConsistencyLoss(nn.Module):
 
 
 class SmoothnessLoss(nn.Module):
+    """Calculate the smoothness loss from disparity.
+
+    This loss function penalises the model for predicting noisy or jumpy
+    disparity maps unnecessarily. Regions in the original image with little
+    change in RGB are weighted higher, and multiplied by the gradient in
+    disparity.
+
+    Therefore, the loss function only penalises jagged disparity when there
+    is no indication of a line or edge in the original image.
+
+    Based off:
+        https://arxiv.org/abs/1609.03677
+    """
     def __init__(self) -> None:
         super().__init__()
 
     def gradient_x(self, x: Tensor) -> Tensor:
+        """Calculate the image gradient along x."""
         # Pad input to keep output size consistent
         x = F.pad(x, (0, 1, 0, 0), mode='replicate')
         return x[:, :, :, :-1] - x[:, :, :, 1:]
 
     def gradient_y(self, x: Tensor) -> Tensor:
+        """Calculate the image gradient along y."""
         # Pad input to keep output size consistent
         x = F.pad(x, (0, 0, 0, 1), mode='replicate')
         return x[:, :, :-1, :] - x[:, :, 1:, :]
 
     def smoothness_weights(self, image_gradient: Tensor) -> Tensor:
+        """Evaluate the weightings according the original image gradient."""
         return torch.exp(-image_gradient.abs().mean(dim=1, keepdim=True))
 
     def smoothness_loss(self, disparity: Tensor, image: Tensor) -> Tensor:
+        """Calculate the smoothness loss between an image and the disparity.
+
+        Args:
+            disparity (Tensor): The (single-channel) disparity of the image.
+            image (Tensor): The original image used to predict disparity.
+
+        Returns:
+            Tensor: The per-pixel smoothness loss of the disparity image.
+        """
         disp_grad_x = self.gradient_x(disparity)
         disp_grad_y = self.gradient_y(disparity)
 
@@ -109,6 +195,15 @@ class SmoothnessLoss(nn.Module):
         return smoothness_x.abs() + smoothness_y.abs()
 
     def forward(self, disp: Tensor, images: Tensor) -> Tensor:
+        """Calculate the smoothness loss between the images and disparities.
+
+        Args:
+            disp (Tensor): The stereo disparity prediction.
+            images (Tensor): The original stereo images.
+
+        Returns:
+            Tensor: The smoothness loss as a single float.
+        """
         smooth_left_loss = self.smoothness_loss(disp[:, 0:1], images[:, 0:3])
         smooth_right_loss = self.smoothness_loss(disp[:, 1:2], images[:, 3:6])
 
@@ -116,12 +211,30 @@ class SmoothnessLoss(nn.Module):
 
 
 class PerceptualLoss(nn.Module):
+    """Calculate the discriminator feature reconstruction loss.
+
+    This loss compares the reconstructed and original images by calculating
+    the L1 Loss between their respective feature maps at each encoder stage
+    of the discriminator.
+
+    Based off:
+        https://tinyurl.com/23jb9tnz
+    """
     def __init__(self) -> None:
         super().__init__()
 
     def forward(self, image_pyramid: ImagePyramid,
                 recon_pyramid: ImagePyramid, disc: Module) -> Tensor:
+        """Calculate the perceptual loss of the reconstructed images.
 
+        Args:
+            image_pyramid (ImagePyramid): The original stereo images.
+            recon_pyramid (ImagePyramid): The reconstructed stereo images.
+            disc (Module): The discriminator model to use.
+
+        Returns:
+            Tensor: The total perceptual loss as a single float.
+        """
         perceptual_loss = 0
 
         image_maps = disc.features(image_pyramid)
@@ -133,24 +246,72 @@ class PerceptualLoss(nn.Module):
         return perceptual_loss
 
 
-class AdversarialLoss(nn.Module):
+class GeneratorLoss(nn.Module):
+    """Calculate the loss from failing to create realistic looking images.
+
+    The Generator needs to learn to convince the Discriminator that its
+    reconstructed images are real.
+
+    Therefore, the ground truth values must all be one. The model is then
+    trained on either binary cross-entropy or mean-squared error.
+    """
     def __init__(self, loss: str = 'mse') -> None:
         super().__init__()
 
         self.adversarial = nn.MSELoss() \
             if loss == 'mse' else nn.BCELoss()
 
-    def forward(self, recon_pyramid: ImagePyramid, discriminator: Module,
-                is_fake: bool = True) -> Tensor:
+    def forward(self, recon_pyramid: ImagePyramid,
+                discriminator: Module) -> Tensor:
+        """Calculate the generator loss from the reconstructed images.
 
+        Args:
+            recon_pyramid (ImagePyramid): The reconstructed stereo images.
+            discriminator (Module): The discriminator.
+
+        Returns:
+            Tensor: The generator loss as a single float.
+        """
         predictions = discriminator(recon_pyramid)
-        labels = torch.zeros_like(predictions) \
-            if is_fake else torch.ones_like(predictions)
+        labels = torch.ones_like(predictions)
 
         return self.adversarial(predictions, labels)
 
 
 class TukraLoss(nn.Module):
+    """Calculate the total loss of the model.
+
+    For each scale of the pyramid, the loss is calculated for:
+    - Reconstruction.
+    - Smoothness.
+    - Consistency.
+
+    If adversarial, these are also calculated:
+    - Generator Loss.
+    - Discriminator Feature Reconstruction.
+
+    Code adapted from:
+        https://tinyurl.com/23jb9tnz
+
+    Args:
+        wssim_weight (float, optional): The weight of the reprojection loss.
+            Defaults to 1.0.
+        consistency_weight (float, optional): The weight of the consistency
+            loss. Defaults to 1.0.
+        smoothness_weight (float, optional): The weight of the smooothness
+            loss. Defaults to 1.0.
+        adversarial_weight (float, optional): The weight of the generator
+            loss. Defaults to 0.85.
+        perceptual_weight (float, optional): The weight of the discriminator
+            feature reconstruction loss. Defaults to 0.05.
+        wssim_alpha (float, optional): The weight of SSIM to L1 Loss within
+            the reprojection loss. Defaults to 0.85.
+        perceptual_start (int, optional): The epoch number to begin
+            calculating the discriminator feature reconstruction loss.
+            Defaults to 5.
+        adversarial_loss_type (str, optional): The type of loss function to
+            use for the generator loss. Defaults to 'mse'.
+    """
     def __init__(self, wssim_weight: float = 1.0,
                  consistency_weight: float = 1.0,
                  smoothness_weight: float = 1.0,
@@ -167,7 +328,7 @@ class TukraLoss(nn.Module):
         self.consistency = ConsistencyLoss()
         self.smoothness = SmoothnessLoss()
 
-        self.adversarial = AdversarialLoss(adversarial_loss_type)
+        self.adversarial = GeneratorLoss(adversarial_loss_type)
         self.perceptual = PerceptualLoss()
 
         self.perceptual_start = perceptual_start
@@ -178,11 +339,23 @@ class TukraLoss(nn.Module):
         self.adversarial_weight = adversarial_weight
         self.perceptual_weight = perceptual_weight
 
-    def forward(self, image_pyramid: ImagePyramid,
-                disparities: Tuple[Tensor, ...],
+    def forward(self, image_pyramid: ImagePyramid, disparities: ImagePyramid,
                 recon_pyramid: ImagePyramid, epoch: Optional[int] = None,
                 discriminator: Optional[Module] = None) -> Tensor:
+        """Calculate the total loss of the model.
 
+        Args:
+            image_pyramid (ImagePyramid): The original stereo images.
+            disparities (ImagePyramid): The model disparity predictions
+            recon_pyramid (ImagePyramid): The reconstructed stereo images.
+            epoch (Optional[int], optional): The training epoch (for
+                perceptual start). Defaults to None.
+            discriminator (Optional[Module], optional): The discriminator (if
+                applicable). Defaults to None.
+
+        Returns:
+            Tensor: The total loss as a single float.
+        """
         reprojection_loss = 0
         consistency_loss = 0
         smoothness_loss = 0
