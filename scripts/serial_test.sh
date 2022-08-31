281bc77ca0948320660a60c63b4217b0807a202e
Louis Manestar <louis.manestar@gmail.com>
2022-08-31 12:32:50 +0100

    Removed serial_test.sh

--- scripts/serial_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python main.py config.yml da-vinci --epochs 2 --adversarial \
-    --training-size 16 --validation-size 16 \
-    --save-model-to trained/da-vinci --save-model-every 1 \
-    --save-results-to results/da-vinci --evaluate-every 1 \
-    --home ../
